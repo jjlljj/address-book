5b38b2b2a5cdb71319ef7fb380486caa43fca1bf
jjlljj <logueja@gmail.com>
2019-08-01 08:16:38 -0600

    Fix syntax errors

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from app import db
 
 class Address(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(64), index=true)
+    first_name = db.Column(db.String(64), index=True)
     last_name = db.Column(db.String(64))
     address = db.Column(db.String(128))
     zip_code = db.Column(db.Integer)
@@ -10,4 +10,4 @@ class Address(db.Model):
     state = db.Column(db.String(64))
 
     def __repr__(self):
-        return <Address {}>.format(self.first_name)
+        return '<Address {}>'.format(self.first_name)
