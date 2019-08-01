8fbd4ef5e0cdb48ac65fa87425e7474ed83a84e4
jjlljj <logueja@gmail.com>
2019-07-31 21:58:04 -0600

    WIP Add incomplete address schema

--- app/models.py
+++ app/models.py
@@ -1,2 +1,9 @@
 from app import db
 
+class Address(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(64), index=true)
+    last_name = db.Column(db.String(64), index=true)
+
+    def __repr__(self):
+        return <Address {}>.format(self.username)
