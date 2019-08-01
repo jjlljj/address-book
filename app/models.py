bdcc6290bc7f0a3e448eeb88d206673986289f8d
jjlljj <logueja@gmail.com>
2019-08-01 08:14:53 -0600

    Add models import, migrations

--- app/models.py
+++ app/models.py
@@ -3,7 +3,11 @@ from app import db
 class Address(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(64), index=true)
-    last_name = db.Column(db.String(64), index=true)
+    last_name = db.Column(db.String(64))
+    address = db.Column(db.String(128))
+    zip_code = db.Column(db.Integer)
+    city = db.Column(db.String(64))
+    state = db.Column(db.String(64))
 
     def __repr__(self):
         return <Address {}>.format(self.first_name)
