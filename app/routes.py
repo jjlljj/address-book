3a5144234c578b282ec67804c91d33a884680efd
jjlljj <logueja@gmail.com>
2019-08-01 10:35:27 -0600

    Add get ddb query

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,15 @@
+from flask import render_template
 from app import app
+from app.models import Address
 
 @app.route('/')
 @app.route('/index')
 
 def index():
     return "Hello Flask App"
+
+@app.route('/address_book', methods=['GET'])
+
+def address_book():
+    addresses = Address.query.all()
+    return render_template('address_book.html', addresses=addresses)
