d8a9be1d767aebc44547fe6af37dc8c8898ffdb5
jjlljj <logueja@gmail.com>
2019-08-02 12:14:40 -0600

    Write address record on form submit

--- app/forms.py
+++ app/forms.py
@@ -5,8 +5,8 @@ from wtforms.validators import DataRequired
 class NewAddressForm(FlaskForm):
    first_name = StringField('First Name', validators=[DataRequired()]) 
    last_name = StringField('Last Name', validators=[DataRequired()]) 
-   addresss = StringField('Address', validators=[DataRequired()]) 
+   address = StringField('Address', validators=[DataRequired()]) 
    city = StringField('City', validators=[DataRequired()]) 
    state = StringField('State', validators=[DataRequired()]) 
    zip_code = StringField('Zip_code', validators=[DataRequired()]) 
-   submit = SubmitField('Add Address')
+   submit = SubmitField('submit')
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
-from flask import render_template
-from app import app
+from flask import render_template, flash, redirect, url_for
+from app import app, db
 from app.models import Address
 from app.forms import NewAddressForm
 
@@ -19,4 +19,23 @@ def address_book():
 @app.route('/address_book/new', methods=['GET', 'POST'])
 def add_address():
     form=NewAddressForm()
+    if form.validate_on_submit():
+
+
+        address = Address(
+            first_name = form.first_name.data,
+            last_name = form.last_name.data,
+            address = form.address.data,
+            city = form.city.data,
+            state = form.state.data,
+            zip_code = form.zip_code.data,
+        )
+        db.session.add(address)
+        db.session.commit()
+        flash('New Address Added')
+            # form.first_name.data, 
+            # form.last_name.data
+        return redirect(url_for('address_book'))
+
     return render_template('new_address.html', form=form)
+
