022fa9fe4c482b6c3564cd5c1972e1ea99349599
jjlljj <logueja@gmail.com>
2019-08-03 09:25:24 -0600

    Add favicons, minor styling updates

--- app/forms.py
+++ app/forms.py
@@ -3,11 +3,11 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired 
 
 class AddressForm(FlaskForm):
-   first_name = StringField('First Name', validators=[DataRequired()]) 
-   last_name = StringField('Last Name', validators=[DataRequired()]) 
-   address = StringField('Address', validators=[DataRequired()]) 
-   city = StringField('City', validators=[DataRequired()]) 
-   state = StringField('State', validators=[DataRequired()]) 
-   zip_code = StringField('Zip Code', validators=[DataRequired()]) 
-   submit = SubmitField('Save')
+    first_name = StringField('First Name:', validators=[DataRequired()]) 
+    last_name = StringField('Last Name:', validators=[DataRequired()]) 
+    address = StringField('Address:', validators=[DataRequired()]) 
+    city = StringField('City:', validators=[DataRequired()]) 
+    state = StringField('State:', validators=[DataRequired()]) 
+    zip_code = StringField('Zip Code:', validators=[DataRequired()]) 
+    submit = SubmitField('Save')
 
